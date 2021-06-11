80a08888c5ec003a114010a34dc0f544cf231c3e
Corrinne W <corrinneworden@gmail.com>
2021-06-11 00:02:02 -0400

    added gh-pages

--- employee-directory/src/index.js
+++ employee-directory/src/index.js
@@ -4,4 +4,7 @@ import App from './App';
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+var ghpages = require('gh-pages')
+ghpages.publish('src', function(err) {});
+
 ReactDOM.render(<App />, document.getElementById('root'));
